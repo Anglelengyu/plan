2f114c0f5cd0bf06258d4d3d3e5fdf87926abb8e
kuangsha <kuangsha@bicitech.cn>
2019-11-21 18:00:06 +0800

    Initial commit

---
+++ planServer/src/main/java/cn/shuhan/plan/PlanApplication.java
@@ -0,0 +1,11 @@
+package cn.shuhan.plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlanApplication.class, args);
+    }
+}
---
+++ planVue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!--<img src="./assets/logo.png">-->
+    <router-view/>
+    <bottom/>
+  </div>
+</template>
+
+<script>
+  import bottom from "@/components/bottom"
+  import Bottom from "./components/Bottom";
+
+  export default {
+    components: {Bottom},
+    name: 'App'
+  }
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #fa93ee;
+    margin-top: 60px;
+  }
+</style>
